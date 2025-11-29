248eb5d1da8e0f8d66dee69d76003c97157a55f7
G33zius <gourley.ryanw@gmail.com>
2025-11-28 23:44:15 -0700

    Graphics improvements

--- Source/CubismEditor.Target.cs
+++ Source/CubismEditor.Target.cs
@@ -9,7 +9,8 @@ public class CubismEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		// BuildEnvironment = TargetBuildEnvironment.Unique;
 		ExtraModuleNames.Add("Cubism");
 		// bOverrideBuildEnvironment = true;
 	}
